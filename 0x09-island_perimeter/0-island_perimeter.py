d9a8451209144084ed3857cd4b71c43a0259bf43
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-02 20:49:54 +0100

    feat: implement function island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,9 +3,10 @@
 Contains the function 'island_perimeter'
 """
 
+
 def island_perimeter(grid):
     """ Retruns the perimeter of the island described in grid
-    
+
         -- grid is a list of list of integers:
             - 0 represents water
             - 1 represents land
@@ -17,24 +18,25 @@ def island_perimeter(grid):
 
         -- There is only one island (or nothing).
 
-        -- The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+        -- The island doesn’t have “lakes” (water inside that isn’t connected
+            to the water surrounding the island).
     """
     row = len(grid)
     col = len(grid[0])
     perimeter = 0
-    
+
     r = 0
     while r < row:
         c = 0
         while c < col:
             if grid[r][c] == 1:
-                if r == 0 or grid[r-1][c] == 0:
-                    perimeter +=1
-                if c == 0 or grid[r][c-1] == 0:
-                    perimeter +=1
-                if r == row - 1 or grid[r+1][c] == 0:
+                if r == 0 or grid[r - 1][c] == 0:
                     perimeter += 1
-                if c == col -1 or grid[r][c+1] == 0:
+                if c == 0 or grid[r][c - 1] == 0:
+                    perimeter += 1
+                if r == row - 1 or grid[r + 1][c] == 0:
+                    perimeter += 1
+                if c == col - 1 or grid[r][c + 1] == 0:
                     perimeter += 1
             c += 1
         r += 1
