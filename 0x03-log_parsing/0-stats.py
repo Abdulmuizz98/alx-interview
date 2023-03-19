a1f7b3b22a2748b3239c792fa8a57a8ae767971a
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-19 16:47:04 +0100

    fix: pycodestyle fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-"""Script that parses HTTP error logs 
-    for 'GET \/projects\/260 HTTP\/1\.1'
+"""Script that parses HTTP error logs
+    for 'GET /projects/260 HTTP/1.1'
 """
 
 import sys
 import re
 
-lines = sys.stdin.readlines()
+# lines = sys.stdin.readlines()
 file_size = 0
-count = {"200": 0, "301":0, "400": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-
-for i in range(len(lines)):
-    line = lines[i][0:-1]
-    try:
-        if i % 10:
+count = {"200": 0, "301": 0, "400": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+line_count = 0
+try:
+    for line in sys.stdin:
+        line = line[0:-1]
+        if line_count != 0 and line_count % 10 == 0:
             # print what you need print
             print("File size:", file_size)
             [print('{}: {}'.format(k, v)) for k, v in count.items() if v]
-            
-        reg_fmt: str = '\d+\.\d+\.\d+\.\d+ - \[.*\] "GET \/projects\/260 HTTP\/1\.1" \d+ \d+'
-        reg_stcode: str = '(?<=" ).*?(?= \d)'
-        reg_filesz: str = '\d+$'
+
+        first = r'\d+\.\d+\.\d+\.\d+ - \[.*\]'
+        second = r'"GET \/projects\/260 HTTP\/1\.1" \d+ \d+'
+        reg_fmt: str = '{} {}'.format(first, second)
+
+        reg_stcode: str = r'(?<=" ).*?(?= \d)'
+        reg_filesz: str = r'\d+$'
         match_fmt = re.findall(reg_fmt, line)
-        if match_fmt: #use the main regex to check format
-            match_stcode = re.findall(reg_stcode, line) # use the backtracking to get only size
-            if match_stcode and count.get(match_stcode[0]):
+        print('match_fmt:', match_fmt)
+        if match_fmt:  # use the main regex to check format
+            match_stcode = re.findall(reg_stcode, line)  # get size
+            print("match_stcode:", match_stcode)
+            if match_stcode and match_stcode[0] in count:
                 count[match_stcode[0]] += 1
-                file_size += re.findall(reg_filesz, line)[0]
-    except KeyboardInterrupt:
-        # print before you time out
-        print("File size:", file_size)
-        [print('{}: {}'.format(k, v)) for k, v in count.items() if v]
+                file_size += int(re.findall(reg_filesz, line)[0])
+        line_count += 1
+except KeyboardInterrupt:
+    # print before you time out
+    print("File size:", file_size)
+    [print('{}: {}'.format(k, v)) for k, v in count.items() if v]
+    raise
