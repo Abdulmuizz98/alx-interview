07b4cd948c2937c931285b94ede278cc30d70e1c
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-21 07:47:59 +0100

    fix: add ommited status code 401

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-''' This module has been duly documented. this is not too much '''
+''' A script that reads stdin line by line and computes metrics'''
 
 
 if __name__ == '__main__':
-    """ This module has been duly documented. this is not too much """
     import sys
     import re
 
     file_size = 0
     count = {
-        "200": 0, "301": 0, "400": 0, "403": 0, "404": 0, "405": 0, "500": 0
+        "200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0
     }
     line_count = 0
-    
+
     try:
         for line in sys.stdin:
             line = line[0:-1]
             if line_count != 0 and line_count % 10 == 0:
                 print("File size:", file_size)
-                [print('{}: {}'.format(k, v)) for k, v in count.items() if v]
+                [print('{}: {}'.format(
+                       k, v)) for k, v in sorted(count.items()) if v]
 
             first = r'\d+\.\d+\.\d+\.\d+ - \[.*\]'
             second = r'"GET \/projects\/260 HTTP\/1\.1" \d+ \d+'
@@ -27,10 +28,8 @@ if __name__ == '__main__':
             reg_filesz = r'\d+$'
 
             match_fmt = re.findall(reg_fmt, line)
-
             if match_fmt:  # use the main regex to check format
                 match_stcode = re.findall(reg_stcode, line)  # get size
-
                 if match_stcode and match_stcode[0] in count:
                     count[match_stcode[0]] += 1
                     file_size += int(re.findall(reg_filesz, line)[0])
@@ -42,4 +41,5 @@ if __name__ == '__main__':
 
     finally:
         print("File size:", file_size)
-        [print('{}: {}'.format(k, v)) for k, v in count.items() if v]
+        [print('{}: {}'.format(
+               k, v)) for k, v in sorted(count.items()) if v]
