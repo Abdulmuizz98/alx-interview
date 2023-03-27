f7991179362fffaeaea59edce282ff7e861b471a
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-27 21:01:56 +0100

    feat: implement validateUTF8 to validate utf-8 data

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,4 +7,7 @@ Contains the function validUTF8 which validates utf8 data
 def validUTF8(data):
     """Returns true if data is valid utf-8 data
     """
-    return max(data) < 256 and min(data) > -1
+    try:
+        bytes(data).decode('utf-8')
+    except Exception:
+        return False
